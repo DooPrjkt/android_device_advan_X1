9cd2cc780ffd6e0fcbd48d6cc184a0b8fb36f130
Saikrishna1504 <saikrishna26918@gmail.com>
2026-06-19 14:24:50 +0000

    X1: Rename `libformatter` to `libformatter-V33`
    Change-Id: I0213eee78af45b929b1e6410f47850c1dc89667e
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -144,6 +144,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v33.so')
         .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so')
         .replace_needed('libsfplugin_ccodec_utils.so', 'libsfplugin_ccodec_utils-v33.so')
+        .replace_needed('libformatter.so', 'libformatter-v33.so')
         .replace_needed('libui.so', 'libui-v34.so'),
     'vendor/lib64/libcodec2_soft_common-v33.so': blob_fixup()
         .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v33.so')
