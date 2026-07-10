74ab6a830655f3b1efe70ff2176f69c3d253e707
DooPrjkt <gitorrb@gmail.com>
2026-07-11 03:16:50 +0700

    X1: BoardConfig migrate to BOARD_VENDOR_SEPOLICY_DIRS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,7 +162,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Recovery
